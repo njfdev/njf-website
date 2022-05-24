cf91fbd1dc32e911f0d7a8d87d46c190993b7536
Nicholas Fasching <none>
2022-05-24 13:28:48 -0400

    Delete Evyerthing And Add New Next.js Project

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { AppProps } from 'next/app';
-import 'styles/index.css'
-
-function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
-}
-
-export default App;
