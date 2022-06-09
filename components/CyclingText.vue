1d0a7be6621e834806ca1ff0f5a99bbc7ee37d01
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-06-09 15:29:57 +0000

    Update Index Page

---
+++ components/CyclingText.vue
@@ -0,0 +1,33 @@
+<template>
+    <h2 class="mx-auto w-max
+        text-neutral-100 text-2xl md:text-4xl">
+        <transition name="cyclingText">
+            {{ titleText }}
+        </transition>
+    </h2>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            i: 0,
+            myTitles: [
+                "Programmer",
+                "Software Developer",
+                "Computer Expert"
+            ]
+        }
+    },
+    mounted() {
+        setInterval(() => {
+            this.i = (this.i == this.myTitles.length - 1) ? 0 : this.i + 1;
+        }, 2 * 1000)
+    },
+    computed: {
+        titleText() {
+            return this.myTitles[this.i];
+        }
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
-        <h1>Index</h1>
+        <div class="absolute top-0 left-0 w-[100%] h-[100%]
+            bg-gradient-to-b from-yellow-400 via-red-600 to-blue-600
+            flex flex-col justify-center">
+            <h1 class="mx-auto w-max 
+                text-neutral-50 text-4xl md:text-6xl">
+                Nicholas Fasching
+            </h1>
+            <br class="select-none"/>
+            <CyclingText />
+        </div>
     </div>
 </template>
