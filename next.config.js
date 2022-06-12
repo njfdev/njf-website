00277d1126537bbdb5e78445531faf107581a24e
Nicholas Fasching <none>
2022-06-12 11:27:01 -0400

    Fix Next Safe Config

--- next.config.js
+++ next.config.js
@@ -3,27 +3,17 @@ const nextSafe = require('next-safe');
 
 const isDev = process.env.NODE_ENV !== 'production';
 
-const nextConfig = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const nextConfig = {
   async headers() {
     return [
       {
-        source: '/:path',
+        source: '/:path*',
         headers: nextSafe({ isDev }),
       },
     ]
   },
   swcMinify: true,
-  reactStrictMode: true,
-  env: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    FIRE_AUTH_DOMAIN: process.env.FIRE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
-  }
-});
+  reactStrictMode: true
+};
 
 module.exports = nextConfig
