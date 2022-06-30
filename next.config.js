777b2742ffe77369c2b19fefde87b643b0244677
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-06-30 15:39:54 -0400

    Add CSP for Clarity

--- next.config.js
+++ next.config.js
@@ -12,8 +12,8 @@ const config = {
           contentSecurityPolicy: {
             "style-src": "'self' 'unsafe-inline' https://fonts.googleapis.com",
             "font-src": "'self' https://fonts.gstatic.com",
-            "script-src": "'self' 'unsafe-eval' https://umami.njf.dev 'sha256-+n6qCniDL16DO3FYEyaFapnzcUr0xSMx30D68AgajH4=' 'sha256-lw/hDXi7c09nitSfC7ys6wF1BXBxNryDqXKYkArrXC4='",
-            "connect-src": "'self' http://localhost:54321 https://umami.njf.dev https://sikyjryusjyiiehfhxmp.supabase.co",
+            "script-src": "'self' 'unsafe-eval' https://umami.njf.dev 'sha256-fXtrHXzZuzWbrMyDp8pSAjWX2EJfMYKzeZxPtnAWO/8=' 'sha256-+n6qCniDL16DO3FYEyaFapnzcUr0xSMx30D68AgajH4=' 'sha256-lw/hDXi7c09nitSfC7ys6wF1BXBxNryDqXKYkArrXC4='",
+            "connect-src": "'self' http://localhost:54321 https://umami.njf.dev https://sikyjryusjyiiehfhxmp.supabase.co https://www.clarity.ms",
             "frame-src": "'self'",
           },
         }),
