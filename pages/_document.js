6967f459eb0c0db1c49cbd8355e70b69181503d9
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-06-30 10:58:02 -0400

    Create Document File to Add Umami Analytics

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+function Document() {
+  return (
+    <Html lang='en'>
+      <Head>
+        <script async defer data-website-id="b9620b9a-f791-4343-9805-f5779446aedf" src="https://umami.njf.dev/umami.js" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default Document;
