8b10c9e79fe15f05218348bc93afade86b590907
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-06-30 16:02:16 -0400

    Use Different Clarity Tracking Code

--- pages/_document.js
+++ pages/_document.js
@@ -5,14 +5,15 @@ function Document() {
     <Html lang='en'>
       <Head>
         <script async defer data-website-id="b9620b9a-f791-4343-9805-f5779446aedf" src="https://umami.njf.dev/umami.js" />
-        <script type="text/javascript">
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "ckksdl5jx1");
-          `}
+    
+        <script>
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "cklc4qoyfi");
+        `}
         </script>
       </Head>
       <body>
