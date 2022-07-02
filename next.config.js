46e45a57e0b4f4a9ae6e42bda328b1b8979d2ce6
Nicholas Fasching <none>
2022-07-02 16:48:25 -0400

    Remove Clarity

--- next.config.js
+++ next.config.js
@@ -12,8 +12,8 @@ const config = {
           contentSecurityPolicy: {
             "style-src": "'self' 'unsafe-inline' https://fonts.googleapis.com",
             "font-src": "'self' https://fonts.gstatic.com",
-            "script-src": "'self' 'unsafe-eval' https://umami.njf.dev 'sha256-hazlti5/8Rt3rP4lshWee69xqYGy9Gk0D6w3YXZYYZk=' 'sha256-+n6qCniDL16DO3FYEyaFapnzcUr0xSMx30D68AgajH4=' 'sha256-lw/hDXi7c09nitSfC7ys6wF1BXBxNryDqXKYkArrXC4=' https://*.clarity.ms https://c.bing.com",
-            "connect-src": "'self' https://umami.njf.dev https://sikyjryusjyiiehfhxmp.supabase.co https://*.clarity.ms https://c.bing.com",
+            "script-src": "'self' 'unsafe-eval' https://umami.njf.dev 'sha256-+n6qCniDL16DO3FYEyaFapnzcUr0xSMx30D68AgajH4=' 'sha256-lw/hDXi7c09nitSfC7ys6wF1BXBxNryDqXKYkArrXC4='",
+            "connect-src": "'self' https://umami.njf.dev https://sikyjryusjyiiehfhxmp.supabase.co",
             "frame-src": "'self'",
           },
         }),
--- pages/_document.js
+++ pages/_document.js
@@ -5,16 +5,6 @@ function Document() {
     <Html lang='en'>
       <Head>
         <script async defer data-website-id="b9620b9a-f791-4343-9805-f5779446aedf" src="https://umami.njf.dev/umami.js" />
-    
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "cklc4qoyfi");
-          `
-        }} />
       </Head>
       <body>
         <Main />
