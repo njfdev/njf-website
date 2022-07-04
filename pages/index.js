76068db5c17ce246ab21abce75a532b46a20c1e6
Nicholas Fasching <none>
2022-07-04 10:38:00 -0400

    Fix Index SEO

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,11 @@ function Home() {
   return (
     <>
       <Head>
-        <title>In Development</title>
+        <title>Website In Development | njf</title>
+        <meta name='description' content="This is the website of Nicholas Fasching. Please come back later because this website is still in development." />
       </Head>
 
+      <h1>In Development - Come Back Later</h1>
       <p>
         Hello and welcome to the website of Nicholas Fasching that is currently in development. Hopefully next time you return, I will have updated this page!
       </p>
