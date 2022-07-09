1b09aaa1b7ab90d8b51e6f3b870eaea23861dc77
Nicholas Fasching <none>
2022-07-09 17:22:47 -0400

    Add Matomo Tracking Code

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import { init } from "@socialgouv/matomo-next";
+
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }, []);
+  
+  return <Component {...pageProps} />;
+}
