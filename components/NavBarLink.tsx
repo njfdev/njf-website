9f4d1addc783f182d7b8ed32247e4c312a167419
Nicholas Fasching <none>
2022-07-12 19:44:43 -0400

    Disable Typescript Errors

--- components/NavBarLink.tsx
+++ components/NavBarLink.tsx
@@ -39,10 +39,22 @@ function NavBarLink({ href, icon, children, containerClass, textClass, onClick }
         <div className={`flex select-none h-max ${containerClass}`} onClick={onClick}>
             <Link href={href}>
                 <div className="flex cursor-pointer">
-                    {icon ? <Icon path={icon} size={screenSize == ScreenSizes.small ? 1.5 : 1} className="text-stone-800 dark:text-stone-300 my-auto" /> : <></>}
+                    {icon ? 
+                        <Icon path={icon} size={
+                            // @ts-ignore
+                            screenSize == ScreenSizes.small ? 1.5 : 1} 
+                            className="text-stone-800 dark:text-stone-300 my-auto" />
+                        :
+                        <></>
+                    }
                     <div className="flex flex-col">
                         <div className="my-auto" />
-                        <span className={`text-neutral-800 dark:text-neutral-300 text-lg text-center align-middle font-extrabold ${textClass || (screenSize == ScreenSizes.small ? '!text-3xl' : '')}`}>
+                        <span className={`text-neutral-800 dark:text-neutral-300 text-lg text-center align-middle font-extrabold
+                            ${textClass ||
+                                // @ts-ignore
+                                (screenSize == ScreenSizes.small ? '!text-3xl' : '')}`
+                            }
+                        >
                             {children}
                         </span>
                         <div className="my-auto" />
