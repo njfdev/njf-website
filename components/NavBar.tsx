bc523280f29c7654ff45fc6d85f21253d13a25f7
Nicholas Fasching <none>
2022-07-13 15:44:30 -0400

    I really want MacOS to test iOS

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -31,8 +31,8 @@ export default function NavBar() {
                 <div className="flex justify-end md:hidden text-neutral-300 z-[1000] w-0 grow">
                     <IconButton icon={navBarOpened ? mdiClose : mdiMenu} onClick={toggleNavBar} />
                 </div>
-                <div className={`bg-neutral-700 absolute md:!hidden top-0 left-0 z-[999] w-screen h-[calc(100vh_+_env(safe-area-inset-bottom))] ${navBarOpened ? "fixed" : "hidden"}`}>
-                    <div className="w-full flex flex-col items-start gap-[20px] pb-[env(safe-area-inset-bottom)] p-10">
+                <div className={`bg-neutral-700 absolute md:!hidden top-0 left-0 z-[999] w-screen min-h-[-webkit-fill-available] ${navBarOpened ? "fixed" : "hidden"}`}>
+                    <div className="w-full flex flex-col items-start gap-[20px] pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] p-10">
                         <div className="my-auto" />
                         <MiddleLinks onClick={toggleNavBar} />
                         <EndLinks onClick={toggleNavBar} />
