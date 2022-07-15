7d7ce947f124b30f3a8b53aa57484c5bffff5479
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-07-15 16:42:52 -0400

    Fix Supabase Setup

--- components/blog/BlogPreview.jsx
+++ components/blog/BlogPreview.jsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 import { H2, H3, P } from 'components/CustomTags'
 import { useRouter } from "next/router";
 
-export default function BlogPreview({ title, description, publish_date, thumbnail, link, aboveFold, disableImage, onClick }) {
+export default function BlogPreview({ title, description, publish_date, thumbnail, link, aboveFold, disableImage = false, onClick }) {
     const router = useRouter()
 
     const formatted_date = new Date(publish_date).toLocaleDateString()
 
     return <li className="flex flex-col gap-2 bg-neutral-800 rounded-xl h-full p-5 cursor-pointer" onClick={link ? () => {router.push(link)} : onClick}>
-        {disableImage &&
+        {!disableImage &&
             <div className="relative flex flex-col justify-center h-[55%] md:h-[65%] w-full bg-neutral-700">
-                {thumbnail && 
-                    <Image src={thumbnail || ''} layout='fill' objectFit='cover' alt="image" priority={aboveFold} /> ||
+                {(thumbnail && thumbnail !== '') && 
+                    <Image src={thumbnail} layout='fill' objectFit='cover' alt="image" priority={aboveFold} /> ||
                     <H2 className='w-max mx-auto'>No Image</H2>
                 }
             </div>
--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const config = {
             "style-src": "'self' 'unsafe-inline' https://fonts.googleapis.com",
             "font-src": "'self' https://fonts.gstatic.com",
             "script-src": `https://${process.env.NEXT_PUBLIC_CLERK_FRONTEND_API} 'self' 'unsafe-eval' https://umami.njf.dev 'sha256-+n6qCniDL16DO3FYEyaFapnzcUr0xSMx30D68AgajH4=' 'sha256-lw/hDXi7c09nitSfC7ys6wF1BXBxNryDqXKYkArrXC4=' 'sha256-HR7DoDjT5hh9RbbFZI/aUcpwqMMH4bDdonu+zKO/MjY='`,
-            "connect-src": `'self' https://${process.env.NEXT_PUBLIC_CLERK_FRONTEND_API} http://localhost:54321 https://umami.njf.dev https://sikyjryusjyiiehfhxmp.supabase.co`,
+            "connect-src": `'self' ws: https://${process.env.NEXT_PUBLIC_CLERK_FRONTEND_API} http://localhost:54321 https://umami.njf.dev https://sikyjryusjyiiehfhxmp.supabase.co`,
             "frame-src": `'self' https://${process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}`,
             "img-src": `'self' https://images.clerk.dev/static/github.svg https://sikyjryusjyiiehfhxmp.supabase.co`,
           },
