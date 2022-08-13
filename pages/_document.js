2c7f15633b5cb0ee2c19e58d659df9993b53d478
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-08-13 09:38:12 -0400

    Add New Font

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,10 @@ function Document() {
     <Html lang='en'>
       <Head>
         <script async defer data-website-id="50e05f5f-156a-447f-923a-97397d16e00a" src="https://umami.njf.dev/umami.js" />
-        
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
-        <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com"/> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/> 
+        <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"/>
       </Head>
       <body className='bg-neutral-100 dark:bg-neutral-900'>
         <Main />
