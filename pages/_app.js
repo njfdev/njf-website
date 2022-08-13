deb0918f57e34e8d49c549619ac05eb614d4fb3b
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-08-13 10:53:24 -0400

    Improve Visual Appearance

--- pages/_app.js
+++ pages/_app.js
@@ -114,6 +114,7 @@ export default function App({ Component, pageProps }) {
                 animate={{ opacity: 1, transition: { delay: 0.05 } }}
                 exit={{ opacity: 0 }}
                 transition={{ duration: 0.2 }}
+                className="rela"
                 >
                   <Component {...pageProps} />
               </motion.div>
