803d1df0f1513ee787c7c2d45486093f4a02a0f5
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-08-31 16:27:39 -0400

    Add Styling And Color of the Day

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { init } from "@socialgouv/matomo-next";
+import "../styles/global.css";
 
 const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
 const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Head from "next/head";
+import style from "./index.module.css";
+import { MD5 } from "crypto-js";
 
 function Home() {
+  const dateString = (new Date).toDateString();
+
+  const date = (new Date).toLocaleDateString('en-us');
+  const hashed = MD5(date).toString();
+  const hexColor = hashed.substring(0, 6);
+
   return (
     <>
       <Head>
@@ -10,8 +18,16 @@ function Home() {
 
       <h1>In Development - Come Back Later</h1>
       <p>
-        Hello and welcome to the website of Nicholas Fasching that is currently in development. Hopefully next time you return, I will have updated this page!
+        Hello and welcome to the website of Nicholas Fasching that is currently in development. While you are waiting for this website to become active, look below ↓.
       </p>
+
+      <h2>Color of the Day:</h2>
+      <div id={style.colorDiv} style={{ backgroundColor: `#${hexColor}` }}>
+        <div id={style.textOverlay}>
+          <h3>{dateString}</h3>
+          <p>Hex: #{hexColor.toUpperCase()}</p>
+        </div>
+      </div>
     </>
   )
 }
