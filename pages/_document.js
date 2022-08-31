6d4e40ed1b3fbe5188b0f1b3809fcf34679df370
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-08-31 15:51:50 -0400

    Clean Up Setup

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <script async defer data-website-id="b9620b9a-f791-4343-9805-f5779446aedf" src="https://umami.njf.dev/umami.js" />
       </Head>
       <body>
         <Main />
