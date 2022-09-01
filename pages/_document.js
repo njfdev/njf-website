d488865460d0027e45841d1d75d855b86ebefd9a
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-09-01 16:14:50 -0400

    Add Meta Viewport Tag (For Mobile Friendliness)

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ function Document() {
   return (
     <Html lang='en'>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </Head>
       <body>
         <Main />
