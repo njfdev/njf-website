f20033f968c89bb372bc8d4c0a1319f07b4227cb
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-09-01 16:13:39 -0400

    Switch to Self Hosted Fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { init } from "@socialgouv/matomo-next";
 import "../styles/global.css";
+import "../styles/fonts.css";
 
 const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
 const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,6 @@ function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/> 
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/> 
-        <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
