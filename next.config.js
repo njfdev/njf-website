5cf3f007c050688131b0784ce83056529e7f3c7e
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-09-02 10:00:57 -0400

    Add Sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextSafe = require('next-safe');
+const { withSentryConfig } = require('@sentry/nextjs');
 
 const isDev = process.env.NODE_ENV !== 'production';
 
@@ -13,15 +14,22 @@ const config = {
             "style-src": "'self' 'unsafe-inline' https://fonts.googleapis.com",
             "font-src": "'self' https://fonts.gstatic.com",
             "script-src": "'self' 'unsafe-eval' https://matomo.njf.dev https://umami.njf.dev 'sha256-+n6qCniDL16DO3FYEyaFapnzcUr0xSMx30D68AgajH4=' 'sha256-lw/hDXi7c09nitSfC7ys6wF1BXBxNryDqXKYkArrXC4='",
-            "connect-src": "'self' https://umami.njf.dev https://sikyjryusjyiiehfhxmp.supabase.co https://matomo.njf.dev ws:",
+            "connect-src": "'self' https://umami.njf.dev https://sikyjryusjyiiehfhxmp.supabase.co https://matomo.njf.dev https://*.ingest.sentry.io ws:",
             "frame-src": "'self'",
           },
         }),
       },
     ]
   },
+  sentry: {
+    hideSourceMaps: true,
+  },
   swcMinify: !isDev,
   reactStrictMode: true
 };
 
-module.exports = config
+const sentryWebpackPluginOptions = {
+  silent: true,
+};
+
+module.exports = withSentryConfig(config, sentryWebpackPluginOptions);
