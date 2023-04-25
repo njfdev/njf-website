d75dbf0381cd2e89169f4703549b08b4a1029efc
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2023-04-24 20:39:25 -0400

    Add Color of the Day Easter Egg

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,40 @@ import localFont from "next/font/local";
 import { Url } from "next/dist/shared/lib/router/router";
 import SocialActivity from "@/components/SocialActivity";
 import { Suspense } from "react";
+import Script from "next/script";
+import { MD5 } from "crypto-js";
+import { GetColorName } from "hex-color-to-color-name";
 
 const GoodDogNew = localFont({ src: "gooddog-new.woff2" });
 
 export default function Home() {
+  const date = new Date().toLocaleDateString("en-us");
+  const hashed = MD5(date).toString();
+  const hexColor = hashed.substring(0, 6);
+
+  const colorName = GetColorName(hexColor);
+
   return (
     <main className="100vw sm:p-8 p-2">
+      <Script>{`
+      let keylog = "";
+      let specialPhrase = "cotd"
+      document.onkeydown = function (e) {
+          let keypressed = e.key;
+          keylog += keypressed;
+          if (!specialPhrase.startsWith(keylog)) {
+            keylog = ""
+            if (keypressed === specialPhrase[0]) {
+              keylog += keypressed
+            }
+          }
+          if (keylog === specialPhrase) {
+            document.getElementById("cotd").classList.remove("hidden")
+            keylog = ""
+          }
+      };
+      `}</Script>
+
       <div className="max-w-3xl mx-auto flex flex-col gap-4">
         <Image
           src={logoIcon}
@@ -70,10 +98,29 @@ export default function Home() {
         >
           Recent Activity
         </h2>
-        {}
         <Suspense fallback={<div>Loading...</div>}>
           <SocialActivity />
         </Suspense>
+      </div>
+      <div
+        id="cotd"
+        className="hidden mt-4 mx-auto flex flex-col justify-center items-center align-middle max-w-xl"
+      >
+        <h2
+          className={`w-max text-5xl mb-4 ${GoodDogNew.className} dark:text-white text-black`}
+        >
+          Color of the Day
+        </h2>
+        <div
+          className="w-full text rounded-3xl aspect-square flex flex-col justify-center items-center align-middle"
+          style={{ backgroundColor: `#${hexColor}` }}
+        >
+          <div className="bg-[rgba(0,0,0,0.25)] text-white rounded-xl backdrop-blur p-8">
+            <h3 className={`text-4xl ${GoodDogNew.className}`}>{colorName}</h3>
+            <h4 className="text-xl mb-1">{date}</h4>
+            <p>#{hexColor}</p>
+          </div>
+        </div>
       </div>
     </main>
   );
