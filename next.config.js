164c94a1e2821ee0c4bd9ef5dce5edeea2ea7e54
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2025-08-19 11:01:56 -0400

    Remove All Analytics Packages (Sentry, OpenReplay, and Matomo)

--- next.config.js
+++ next.config.js
@@ -1,37 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextSafe = require('next-safe');
-const { withSentryConfig } = require('@sentry/nextjs');
+const nextSafe = require("next-safe");
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== "production";
 
 const config = {
   async headers() {
     return [
       {
-        source: '/:path*',
-        headers: nextSafe({ 
+        source: "/:path*",
+        headers: nextSafe({
           contentSecurityPolicy: {
-            "style-src": "'self' 'unsafe-inline' https://fonts.googleapis.com https://static.openreplay.com",
+            "style-src":
+              "'self' 'unsafe-inline' https://fonts.googleapis.com https://static.openreplay.com",
             "font-src": "'self' https://fonts.gstatic.com",
-            "script-src": "'self' 'unsafe-eval' https://matomo.njf.dev https://umami.njf.dev 'sha256-+n6qCniDL16DO3FYEyaFapnzcUr0xSMx30D68AgajH4=' 'sha256-lw/hDXi7c09nitSfC7ys6wF1BXBxNryDqXKYkArrXC4='",
-            "connect-src": "'self' wss: https://static.openreplay.com https://umami.njf.dev https://sikyjryusjyiiehfhxmp.supabase.co https://matomo.njf.dev https://*.ingest.sentry.io https://api.openreplay.com ws:",
+            "script-src":
+              "'self' 'unsafe-eval' https://matomo.njf.dev https://umami.njf.dev 'sha256-+n6qCniDL16DO3FYEyaFapnzcUr0xSMx30D68AgajH4=' 'sha256-lw/hDXi7c09nitSfC7ys6wF1BXBxNryDqXKYkArrXC4='",
+            "connect-src":
+              "'self' wss: https://static.openreplay.com https://umami.njf.dev https://sikyjryusjyiiehfhxmp.supabase.co https://matomo.njf.dev https://*.ingest.sentry.io https://api.openreplay.com ws:",
             "frame-src": "'self'",
             "worker-src": "'self' blob:",
             "media-src": "'self' https://static.openreplay.com",
           },
         }),
       },
-    ]
-  },
-  sentry: {
-    hideSourceMaps: true,
+    ];
   },
   swcMinify: !isDev,
-  reactStrictMode: true
+  reactStrictMode: true,
 };
 
-const sentryWebpackPluginOptions = {
-  silent: true,
-};
-
-module.exports = withSentryConfig(config, sentryWebpackPluginOptions);
+module.exports = config;
--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,13 @@
 import React, { useEffect } from "react";
-import { init } from "@socialgouv/matomo-next";
 import "../styles/global.css";
 import "../styles/fonts.css";
 import Head from "next/head";
-import Tracker from '@openreplay/tracker/cjs';
-
-const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
-const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
-const OPEN_REPLAY_PROJECT_KEY = process.env.NEXT_PUBLIC_OPEN_REPLAY_PROJECT_KEY;
-
-const tracker = new Tracker({
-  projectKey: OPEN_REPLAY_PROJECT_KEY,
-  __DISABLE_SECURE_MODE: process.env.NODE_ENV === 'development',
-})
 
 export default function App({ Component, pageProps }) {
-  let debounce = false;
-  useEffect(() => {
-    (async () => {
-      if (!debounce) {
-        const trackerAssist = (require('@openreplay/tracker-assist/cjs')).default;
-        tracker.use(trackerAssist({}));
-        tracker.start();
-        init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
-        debounce = true;
-      }
-    })();
-  }, []);
-  
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
     </>
